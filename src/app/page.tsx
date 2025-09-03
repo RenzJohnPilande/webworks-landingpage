8fba95b8a26f9c9b64c3e0c1c22c2283722e262d
RenzJohnPilande <renzjohnpilande@gmail.com>
2025-09-03 14:17:27 +0800

    images update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@
 
 import Link from "next/link";
 import { FaCheckCircle } from "react-icons/fa";
-import { FaClock } from "react-icons/fa6";
 import { MdOutlineTimer } from "react-icons/md";
 
 
@@ -10,12 +9,13 @@ export default function Home() {
   return (
     <div className="flex w-full flex-wrap items-start justify-center">
       {/* Hero Banner */}
-      <div className="flex flex-wrap w-full justify-center items-center px-10 py-40 text-white bg-slate-950">
-        <div className="flex flex-wrap container max-w-[1200px]">
+      <div className="relative flex flex-wrap w-full justify-center items-center px-10 py-40 text-white bg-[url(/images/herobanner.jpg)] bg-cover bg-center">
+        <div className="absolute inset-0 bg-linear-to-r from-[#001B3B] to-slate-900/10 z-0"></div>
+        <div className="flex flex-wrap container max-w-[1200px] z-100">
           <div className="flex flex-wrap max-w-[600px] gap-8">
             <h1 className="text-4xl md:text-6xl font-bold">Effortless Web Hosting Starts Here</h1>
             <p className="text-sm md:text-lg">Free Domain, Professional Email, and Privacy Protection included</p>
-            <Link href="/" className="px-5 py-3 bg-blue-500 hover:bg-blue-400">Get Started</Link>
+            <Link href="/" className="px-5 py-3 transition delay-150 duration-200 ease-in-out hover:-translate-y-2 bg-blue-500 hover:bg-white hover:text-slate-900">Get Started</Link>
           </div>
         </div>
       </div>
@@ -75,36 +75,39 @@ export default function Home() {
         
       </div>
       {/* Why Choose Us */}
-      <div className="grid w-full grid-cols-1 lg:grid-cols-2">
-       <div className="flex flex-wrap lg:justify-end w-full bg-slate-200 py-15">
-         <div className="flex flex-col flex-wrap justify-center content-center px-10 max-w-[400px] gap-4">
+      <div className="grid w-full grid-cols-1 lg:grid-cols-2 items-center">
+       <div className="flex flex-wrap justify-center w-full bg-slate-200 py-15">
+        <div className="flex flex-wrap">
+          <img src="/images/our-feature-img.png" alt="Our features image" className="w-full h-auto transition duration-200 ease-in-out hover:-translate-y-2"/>
+        </div>
+        <div className="flex flex-col flex-wrap justify-center content-center px-10 max-w-[400px] gap-4">
           <p className="text-blue-500 font-bold">Our Features</p>
           <h4 className="text-slate-900 text-3xl font-bold">Why Choose Us?</h4>
           <p className="text-gray-500 text-sm">Our web hosting solutions are an easy way to get a great website up and running, with so many robust features we throw in a domain, SSl Certificate, professional email address, and privacy protection for free.</p>
-          <Link href="/" className="text-slate-900 border border-slate-900 w-fit px-8 py-3 text-sm font-semibold hover:border-slate-600 hover:text-slate-600">Get Started Today!</Link>
+          <Link href="/" className="text-slate-900 border border-slate-900 w-fit px-8 py-3 text-sm font-semibold transition duration-200 ease-in-out hover:-translate-y-2 hover:border-blue-500 hover:bg-blue-500 hover:text-white">Get Started Today!</Link>
         </div>
        </div>
-        <div className="w-full bg-slate-900 px-10 text-white">
-          <div className="flex flex-wrap flex-col justify-center gap-10 py-15">
-            <div className="flex items-center gap-6 w-full">
-              <div className="flex items-center border p-4 bg-white text-slate-900 "><MdOutlineTimer className="text-3xl"/></div>
-              <div className="flex flex-wrap max-w-[350px] gap-1.5">
-                <h5 className="font-semibold">99% Network Uptime</h5>
-                <p className="text-sm">At hosting maximum performance, we&apos;re dedicated to providing top tier soulutions tailored.</p>
+        <div className="w-full bg-slate-900 px-10 text-white h-full items-center flex">
+          <div className="flex flex-wrap flex-col justify-center gap-2 py-15">
+            <div className="flex items-center gap-6 w-full hover:bg-blue-500 p-4">
+              <div className="flex items-center border p-5 bg-white text-slate-900 "><MdOutlineTimer className="text-3xl"/></div>
+              <div className="flex flex-wrap max-w-[500px] gap-1.5">
+                <h5 className="font-bold text-xl">99% Network Uptime</h5>
+                <p className="text-md">At hosting maximum performance, we&apos;re dedicated to providing top tier soulutions tailored.</p>
               </div>
             </div>
-            <div className="flex items-center gap-6 w-full">
-              <div className="flex items-center border p-4 bg-white text-slate-900 "><MdOutlineTimer className="text-3xl"/></div>
-              <div className="flex flex-wrap max-w-[350px] gap-1.5">
-                <h5 className="font-semibold">99% Network Uptime</h5>
-                <p className="text-sm">At hosting maximum performance, we&apos;re dedicated to providing top tier soulutions tailored.</p>
+            <div className="flex items-center gap-6 w-full hover:bg-blue-500 p-4">
+              <div className="flex items-center border p-5 bg-white text-slate-900 "><MdOutlineTimer className="text-3xl"/></div>
+              <div className="flex flex-wrap max-w-[500px] gap-1.5">
+                <h5 className="font-bold text-xl">99% Network Uptime</h5>
+                <p className="text-md">At hosting maximum performance, we&apos;re dedicated to providing top tier soulutions tailored.</p>
               </div>
             </div>
-            <div className="flex items-center gap-6 w-full">
-              <div className="flex items-center border p-4 bg-white text-slate-900 "><MdOutlineTimer className="text-3xl"/></div>
-              <div className="flex flex-wrap max-w-[350px] gap-1.5">
-                <h5 className="font-semibold">99% Network Uptime</h5>
-                <p className="text-sm">At hosting maximum performance, we&apos;re dedicated to providing top tier soulutions tailored.</p>
+            <div className="flex items-center gap-6 w-full hover:bg-blue-500 p-4">
+              <div className="flex items-center border p-5 bg-white text-slate-900 "><MdOutlineTimer className="text-3xl"/></div>
+              <div className="flex flex-wrap max-w-[500px] gap-1.5">
+                <h5 className="font-bold text-xl">99% Network Uptime</h5>
+                <p className="text-md">At hosting maximum performance, we&apos;re dedicated to providing top tier soulutions tailored.</p>
               </div>
             </div>
           </div>
